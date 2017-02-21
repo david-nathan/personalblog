396c86e1e503a0cd4b4b1f044a4e2cdae5dbc20d
David Nathan <davidnathn@gmail.com>
2017-02-21 21:44:57 +0800

    Initial files

---
+++ index.php
@@ -0,0 +1,54 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>David Nathan</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="blog.css" rel="stylesheet">
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <div class="title">
+        <div id="sidebar"><a href=""><img src="./portrait.jpg" id="me" width="225" itemprop="photo"></a></div>
+
+        <div id="bio">
+            <div style="position:absolute;top:28px;">
+                <h1><span itemprop="name">David Nathan</span></h1>
+
+                <p style="line-height:23px;">
+                Research Associate, Centre for Exploration Targeting, UWA<br> 
+                Email: <a href="mailto:david.nathan@uwa.edu.au">david.nathan@uwa.edu.au</a><br>
+                Office: Room 104, Robert Street Building</p>
+                <p class="external">
+                    <a href="https://github.com/nathad02">Github</a> • <a href="https://scholar.google.com/citations?user=3TXu_OsAAAAJ">Scholar</a> • <a href="https://www.socrates.uwa.edu.au/Staff/StaffProfile.aspx?Person=DavidNathan">Socrates</a></p>
+            </div>
+        </div>
+
+    </div>
+      
+
+      <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  </body>
+</html>    
